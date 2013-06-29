7113facd5c83f5fdcb94942aee421ab4a3ffde18
Sean McQueen <sean@mcqueen.net>
2013-06-29 15:58:38 -0700

    view vines in the client

--- client/grapevine.js
+++ client/grapevine.js
@@ -1,12 +1,33 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to grapevine.";
-  };
+  
+  Meteor.startup(function () {
+    Deps.autorun(function () {
+    Meteor.subscribe('vines', function onComplete() {
+      Session.set('vinesLoaded', true);
+      });
+    });
+  });
 
-  var id = "bTxjnvrueTe"
+
+  function getRandomVineId(){    
+    var v = Vines.find({}).fetch();
+    var rando=Math.floor(Math.random()*11) // random number between 1 and 10..
+    var index = rando % v.length;
+    return v[index].vine_id;
+  }
+
+  // templates
+  Template.hello.greeting = function () {
+      return "Welcome to grapevine.";
+    };
 
   Template.video.vineurl = function () {
-    return "https://vine.co/v/" + id + "/embed/simple";
+    var v = Vines.find({vine_id : "ha7Wbmnq6Ph"}).fetch()[0];
+    if (v){
+      var id = getRandomVineId();
+      return "https://vine.co/v/" + id + "/embed/simple";
+    }
+    // todo: loading url
   }
 
   Template.hello.events({
