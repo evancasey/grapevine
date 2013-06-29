8d52d682d688c21748138561315c581989f3671a
Josh Rael <joshuarael@gmail.com>
2013-06-29 14:23:18 -0700

    render a video on page with ID

--- client/grapevine.js
+++ client/grapevine.js
@@ -3,6 +3,12 @@ if (Meteor.isClient) {
     return "Welcome to grapevine.";
   };
 
+  var id = "bTxjnvrueTe"
+
+  Template.video.vineurl = function () {
+    return "https://vine.co/v/" + id + "/embed/simple";
+  }
+
   Template.hello.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
